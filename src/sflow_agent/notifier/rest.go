5bb6240c16d056153f38f368deffb5a4990647e2
bruceSz <zsyuyizhang@gmail.com>
2015-09-10 00:52:04 +0800

    verified rest post

--- src/sflow_agent/notifier/rest.go
+++ src/sflow_agent/notifier/rest.go
@@ -1,16 +1,40 @@
-package main
+package notifier
 
 import (
-    "net/http" 
+    "net/http"
     "fmt"
+    "bytes"
+    "encoding/json"
 )
 
-func main() {
+func test_req_data() (map[string]string){
+    data := map[string]string{
+       "uuid": "test-zs",
+       "hostname": "test.baidu.com",
+       "in_discard": "-1",
+       "in_error":"-1",
+       "in_bps":"-1",
+       "in_pps": "-1",
+       "out_discard": "-1",
+       "out_error":"-1",
+       "out_bps":"-1",
+       "out_pps":"-1",
+    }
+    return data
+}
+
+func YunhaiPost(data map[string]string) (ret_code int,ret_msg string) {
     client := &http.Client{}
-    req, err := http.NewRequest("GET", 
-                                "http://net-api.yunhai.baidu.com:8855/api/v1/flows/summary/", 
-                                nil)
+    json_data, err := json.Marshal(data)
+    if err != nil {
+        fmt.Println("json err:",err)
+    }
+    body := bytes.NewBuffer([]byte(json_data))
+    req, err := http.NewRequest("POST",
+                                "http://net-api.yunhai.baidu.com:8855/api/v1/flows/summary/",
+                                body)
     req.SetBasicAuth("neutron-api-user","neutron-api-secret")
+
     resp, err := client.Do(req)
     if err != nil {
         fmt.Printf("Error: %s", err)
