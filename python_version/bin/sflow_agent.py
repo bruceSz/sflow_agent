5e07bc225459f0a70c93736773fd810338324e1c
Zhang,Song(OP) <zhangsong06@baidu.com>
2015-10-21 11:46:25 +0800

    add sflow_agent entry point

--- python_version/bin/sflow_agent.py
+++ python_version/bin/sflow_agent.py
@@ -12,8 +12,57 @@ Author: zhangsong06(zhangsong06@baidu.com)
 Date: 2015/10/13 21:03:42
 """
 
-def main():
+import os
+import sys
+
+sys.path.append(
+    os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+
+from sflow_agent import log
+from sflow_agent import client
+from sflow_agent import config
+
+_CONF_FILE = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))),
+                                "etc/sflow_agent.conf")
+
     
+
+
+def main():
+    log.init_log("sflow_agent")
+    config.init(_CONF_FILE)
+    sflow_client = client.client.SflowClient(config.CONF.yunhai)
+    logging.info("Sflow agent start.")
+        
+    def func1():
+        listen_addr = ("0.0.0.0", 6343)
+        sock = socket(AF_INET, SOCK_DGRAM)
+        sock.bind(listen_addr)
+        i = 0
+        while True:
+            data, addr = sock.recvfrom(65535)
+            sflow_datagram = {}
+            sflow_datagram["addr"] = addr
+            sflow_datagram["data"] = data
+            yield sflow_datagram
+            i += 1
+            if i >= 10:
+                break
+    def func3(item):
+        for rec in item:
+            print(rec)
+            #stdout.flush()
+    pipeline = utils.Pipeline(1)
+    pipeline.add_worker(func1)
+    pipeline.add_worker(parser.parse)
+    pipeline.add_worker(func3, tail=True)
+    def kill(signum, frame):
+        pipeline.stop()
+    signal.signal(signal.SIGINT, kill)
+    pipeline.start()
+    pipeline.join()
+    logging.info("Sflow agent end.")
 
 
 if __name__ == "__main__":
